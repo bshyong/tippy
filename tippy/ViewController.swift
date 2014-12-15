246f80a6ae53a6889195e9fe1a6ebf149bf7b150
Benjamin Shyong <ben.shyong@gmail.com>
2014-12-14 18:03:51 -0800

    add gesture recog and segment control

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var tipLabel: UILabel!
   @IBOutlet weak var billField: UITextField!
   @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var tipControl: UISegmentedControl!
   
   
   override func viewDidLoad() {
@@ -25,6 +26,20 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func onEditingChanged(sender: AnyObject) {
+    var tipPercentages = [0.18, 0.2, 0.22]
+    var selectedTipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+    
+    var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+    var tip = billAmount * selectedTipPercentage
+    var total = billAmount + tip
 
+    tipLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+  }
+
+  @IBAction func onTap(sender: AnyObject) {
+    view.endEditing(true)
+  }
 }
 
